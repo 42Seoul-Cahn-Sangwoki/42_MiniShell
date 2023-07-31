2ec1075b591907c8d2b8fd8102516af7996cd9cf
Chiwon Ahn <cahn@c6r8s5.42seoul.kr>
2023-07-31 17:40:35 +0900

    Interface ver.1.2

--- interface.h
+++ interface.h
@@ -6,7 +6,7 @@
 /*   By: cahn <cahn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 16:33:43 by cahn              #+#    #+#             */
-/*   Updated: 2023/07/30 17:38:31 by cahn             ###   ########.fr       */
+/*   Updated: 2023/07/31 17:39:59 by cahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,21 @@
 # include <readline/history.h>
 # include <signal.h>
 
+typedef struct s_file_info
+{
+    char                *file_name; // Null 이면 fd값 확인
+    int                 fd;
+    struct s_file_info  *next;
+}   t_file_info;
+
 typedef struct s_node
 {
-    char            **commands;
-    int             idx;
-    struct s_node	*next;
+    char            **commands; // 딱 명령어 만큼만 할당해주셈
+    t_file_info     *infile_head; // head 더미노드로 할지 말지 알려주셈
+    t_file_info     *outfile_head;
 }   t_node;
 
-void    execute(t_node *cmds, int length); // 배열과 배열 크기
+void    execute(t_node *cmds, int length); // 배열과 배열 크기(크기 꼭 리턴)
 void	print_stderr(char *error_print);
 void	non_valid_error(char *error_print);
 
