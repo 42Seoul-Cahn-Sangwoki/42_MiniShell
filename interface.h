f1cbf253fd37527d5a7e2718926a39834bc12e7e
Sangwon Kim <sangwoki@c6r6s2.42seoul.kr>
2023-08-01 15:57:56 +0900

    change interface.h [macro number, fd]

--- interface.h
+++ interface.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   base.h                                             :+:      :+:    :+:   */
+/*   interface.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sangwoki <sangwoki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/29 17:52:25 by sangwoki          #+#    #+#             */
-/*   Updated: 2023/07/30 14:57:17 by sangwoki         ###   ########.fr       */
+/*   Created: 2023/07/30 16:33:43 by cahn              #+#    #+#             */
+/*   Updated: 2023/08/01 15:57:14 by sangwoki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BASE_H
-# define BASE_H
+#ifndef INTERFACE_H
+# define INTERFACE_H
 
 # include "libft/libft.h"
 # include <signal.h>
@@ -21,24 +21,31 @@
 # include <readline/history.h>
 # include <signal.h>
 
-# define FALSE 0
-# define TRUE 1
-# define CMD 0
-# define OPTION 1
+# define WRITE_NEW 0
+# define WRITE_BACK 1
+# define READ -1
+# define HERE_DOC -2
 
-typedef struct s_state
+// 여기 매크로값 넣어주셈 (> 이거는 0, >> 이거는 1, inputfile이면 -1)
+// open: <, heredoc: <<
+// write: >, append: >> 
+
+typedef struct s_file_info
 {
-	int		is_pipex;
-	int		is_error;
-}	t_state;
+	char				*file_name;
+	int					write_mode;
+	struct s_file_info	*next;
+}	t_file_info;
 
-// command[0] == cmd, command[1] == option
 typedef struct s_node
 {
-	char			*command[2];
-	int				idx;
-	t_state			state;
-	struct s_node	*next;
+	char			**commands;
+	t_file_info		*infile_head;
+	t_file_info		*outfile_head;
 }	t_node;
+
+void	execute(t_node *cmds, int length); // 배열과 배열 크기(크기 꼭 리턴)
+void	print_stderr(char *error_print);
+void	non_valid_error(char *error_print);
 
 #endif
