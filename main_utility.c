3d6c99736438334518ecee432555cf8c5873238f
Chiwon Ahn <cahn@c5r3s5.42seoul.kr>
2023-08-17 19:41:37 +0900

    main_test

---
+++ main_utility.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_utility.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangwoki <sangwoki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 15:43:45 by sangwoki          #+#    #+#             */
+/*   Updated: 2023/08/17 14:21:18 by sangwoki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"interface.h"
+#include"parsing/parsing.h"
+#include"signal/signal.h"
+
+void	ft_execute(t_node **token, int length)
+{
+	int			i;
+	int			cmd_idx;
+	t_file_info	*head1;
+	t_file_info	*head2;
+
+	printf("length : %d\n", length);
+	i = 0;
+	while (token[i])
+	{
+		printf("pipe: ");
+		cmd_idx = 0;
+		while (token[i]->commands[cmd_idx])
+		{
+			printf("[%s] ", token[i]->commands[cmd_idx]);
+			cmd_idx++;
+		}
+		printf("\n");
+		head1 = token[i]->infile_head;
+		if (head1 != 0)
+		{
+			while (head1)
+			{
+				printf("[%s %d]\n", head1->file_name, head1->write_mode);
+				head1 = head1->next;
+			}
+		}
+		printf("\n\n\n");
+		head2 = token[i]->outfile_head;
+		if (head2 != 0)
+		{
+			while (head2)
+			{
+				printf("[%s %d]\n", head2->file_name, head2->write_mode);
+				head2 = head2->next;
+			}
+		}
+		i++;
+	}
+}
+
+void	init_env(int argc, char *argv[], char *envp[])
+{
+	argv = 0;
+	argc = 0;
+	g_global_var.envp_head = copy_env_return_head(envp);
+	g_global_var.exit = 0;
+}
+
+// ft_exeucte -> execute
+void	valid_command_line(char *line)
+{
+	int		length;
+	t_node	**token;
+
+	token = command_line(line, &length);
+	if (token == 0)
+		print_stderr("missing: format");
+	execute_signal();
+	ft_execute(token, length);
+	token_free(&token);
+}
